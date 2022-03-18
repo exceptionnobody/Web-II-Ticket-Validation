68a5e2f048290265f12f51c7c6711b73dc9f3524
Marco Ballario <marco.ballario1997@gmail.com>
2022-03-18 16:42:31 +0100

    Add server module

---
+++ server/src/test/kotlin/com/group12/server/ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.group12.server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
